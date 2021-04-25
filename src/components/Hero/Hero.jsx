be1e7d8edf3b41b31459f5c66fdf2841421c9094
Aryan Pareek <41026325+aryannpareek@users.noreply.github.com>
2021-04-24 19:26:37 -0700

    Fixed loading bug

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,16 +27,16 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || 'Aryan Pareek'}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || "I am a software developer and aspiring entrepreneur"}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta || 'Learn More'}
               </Link>
             </span>
           </p>
