88e98f1feee8db1e84a227246d841f8dd0288ea8
Aryan Pareek <apareek369@gmail.com>
2021-11-04 20:47:06 -0700

    More updates

--- src/mock/data.js
+++ src/mock/data.js
@@ -22,7 +22,7 @@ export const aboutData = {
     'I am currently a junior at UC San Diego pursuing my B.S. degree in Computer Science. Through my prior coursework, personal projects, and internships, I have gained experience with Java, Python, C++, C, Swift, ARM, x86, and Bash. Additionally, I\'m currently learning front-end development (HTML, CSS, and JavaScript).',
   paragraphTwo:
     'Furthermore, working as a leader in my past projects and internships has sharpened my ability to learn in a fast-paced environment, be an effective communicator, and make good technical judgements. I’m result-oriented and strive to solve problems through innovative solutions.',
-  paragraphThree: 'In my free time, you can catch me playing (and watching) numerous sports such as football, basketball, and tennis. I enjoy being outdoors and travelling, whether it\'s hiking at national parks or booking a spontaneous vacation with my freinds. I value new expereinces and getting comfortable with being uncomfortable.',
+  paragraphThree: 'In my free time, you can catch me playing (and watching) numerous sports such as football, basketball, and tennis. I enjoy being outdoors and travelling, whether it\'s hiking at national parks or booking spontaneous vacations with my freinds. I value new expereinces and getting comfortable with being uncomfortable.',
   resume: 'https://docs.google.com/document/d/1DiFH86dFq1e6QAw7why-1j5Oetp2giV6cNMbVAQKLLU/edit', 
   // if no resume, the button will not show up
 };
@@ -35,7 +35,7 @@ export const projectsData = [
     title: 'STAFFing Tool - System for Tracking and Allocating FTEs Forecasting',
     info: 'For my SDE internship project at General Atomics Aeronautical Systems, I developed the first iteration of “STAFFing Tool,” an app that provides access to all General Atomics Aeronautical Systems data sources to enable comprehensive resource planning. The application provides summaries of important data to assist managers and supervisors in making informed budgeting decisions. For example, company staff could utilize STAFFing Tool to see where they need to adjust resource allocation in areas such as labor hours or employee headcount per project. In sum, STAFFing tool saves General Atomics time and money by providing an easy to interpret, aggregate visualization of critical data points.',
     info2: '',
-    url: '', 
+    // url: '', 
     // repo: '', // if no repo, the button will not show up
   },
   {
@@ -53,7 +53,7 @@ export const projectsData = [
     title: 'iOS Mobile Card Game App',
     info: 'An iOS mobile card game app I built using Xcode and Swift. The app consists of popular games such as the "War" card game and "Slots" matching game. Repositories for both games can be found on my GitHub!',
     info2: '',
-    url: '',
+    // url: '',
     // repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
 
